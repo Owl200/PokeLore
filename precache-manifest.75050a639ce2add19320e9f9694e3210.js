c662a7cfa3110c86f036eae582c8711c5672349d
Owl200 <alromaru@hotmail.com>
2022-04-19 09:51:13 -0500

    Updates

---
+++ precache-manifest.75050a639ce2add19320e9f9694e3210.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "75597d265ed7f04793e5",
+    "url": "/PokeLore/static/css/main.a8687db1.chunk.css"
+  },
+  {
+    "revision": "75597d265ed7f04793e5",
+    "url": "/PokeLore/static/js/main.75597d26.chunk.js"
+  },
+  {
+    "revision": "c5e9b8a4f842bef87c22",
+    "url": "/PokeLore/static/js/1.c5e9b8a4.chunk.js"
+  },
+  {
+    "revision": "bfcbb05d0b693b0cebbd",
+    "url": "/PokeLore/static/js/runtime~main.bfcbb05d.js"
+  },
+  {
+    "revision": "2f93f3c016824a3621e4e7ec0474e018",
+    "url": "/PokeLore/index.html"
+  }
+];
