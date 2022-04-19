cdd05257291ec789c4c2a6240158de4938e716a4
Owl200 <alromaru@hotmail.com>
2022-04-19 17:48:04 -0500

    improved responsiveness and added music

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import pokeTypeIcons from './poketypeicons';
 import backgroundColor from './backgroundColor';
 import initial from './baseObject';
 import PokeFlavor from './PokeFlavor';
+import MusicButton from './MusicButton';
 
 function App() {
   const [pokemon, setPokemon] = useState('pikachu');
@@ -37,30 +38,47 @@ function App() {
     setPokemon(pokeData.id + 1)
   }
 
+  const togglePokemon = e => {
+    e.target.src === pokeData.sprites.front_default ?
+    e.target.src = pokeData.sprites.front_shiny :
+    e.target.src = pokeData.sprites.front_default
+  }
+
   const pokeType = pokeData.types.map(element => { 
     return (
       <img className="type-icon" src={pokeTypeIcons[element.type.name]} title={element.type.name} alt=""/>  
     )
   })
 
+
+
   return (
     <div className="App" style={{background: `linear-gradient(135deg, ${backgroundColor[pokeData.types[0].type.name]} 0%, ${pokeData.types[1] ? backgroundColor[pokeData.types[1].type.name] : 'black'} 100%`}}>
       <header className="App-header">
         <h1>PokeLore</h1>
+        <MusicButton />
       </header>
       <div className='card'>
         <input className='search-input' onKeyPress={handleKeyPress} ref={textInput} type='text' placeholder='Search by name or index'/>
         <button className='search-button' onClick={handleClick} >Search</button>
-        <h3>{pokeData.name[0].toUpperCase()}{pokeData.name.slice(1).replaceAll('-', ' ')} ({pokeData.id})</h3> 
-        <img src={pokeData.sprites.front_default} alt={`${pokemon} sprite`}/>
-        <p>Height: {pokeData.height / 10}m</p>
-        <p>Weight: {pokeData.weight / 10}Kg</p>
-        <p>Type:</p>
-        <div className='type-collection'>
-          {pokeType}
+        <div className='flex'>
+          <div className='left'>
+            <button className='nav-button' onClick={prevPoke}>&lt;&lt;</button>
+          </div>
+          <div className='center'>
+            <h3>{pokeData.name[0].toUpperCase()}{pokeData.name.slice(1).replaceAll('-', ' ')} ({pokeData.id})</h3> 
+            <img id='pokemon-img' onClick={togglePokemon} src={pokeData.sprites.front_default} alt={`${pokemon} sprite`}/>
+            <p>Height: {pokeData.height / 10}m</p>
+            <p>Weight: {pokeData.weight / 10}Kg</p>
+            <p>Type:</p>
+            <div className='type-collection'>
+              {pokeType}
+            </div>
+          </div>
+          <div className='right'>
+            <button className='nav-button' onClick={nextPoke}>&gt;&gt;</button>
+          </div>
         </div>
-        <button className='nav-button' onClick={prevPoke}>&lt;&lt;</button>
-        <button className='nav-button' onClick={nextPoke}>&gt;&gt;</button>
         <PokeFlavor id={pokeData.id} />
       </div>
     </div>
---
+++ src/MusicButton.js
@@ -0,0 +1,23 @@
+import React, {useState, useEffect} from 'react';
+import mp3 from './audio/Littleroot.mp3'
+import off from './img/music-off.png';
+import on from './img/music-on.png';
+
+export default function MusicButton() {
+    const [song] = useState(new Audio(mp3))
+    const [isPlaying, setIsPlaying] = useState(false)
+
+    useEffect(() => {isPlaying ? song.play() : song.pause()}, [isPlaying])
+    
+    const toggleMusic = e => {
+        e.target.src === off ? e.target.src = on : e.target.src = off;
+        setIsPlaying(prev => !prev);
+    }
+    
+
+    return (
+        <>
+            <img id='music' onClick={toggleMusic} className='music-img' src={off} alt='music on'/>
+        </>
+    )
+}
